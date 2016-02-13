228da27dc483e17ddbb93702bce98f7ca5cc973b
Byron Williams <byron@swooneditions.com>
2016-02-13 12:16:47 +0000

    Fix merges

--- src/app/routes/home-route.component.ts
+++ src/app/routes/home-route.component.ts
@@ -2,10 +2,9 @@ import {Component, OnInit, EventEmitter} from 'angular2/core';
 import {Routes} from '../config';
 import {Router, RouteParams, Location, ROUTER_DIRECTIVES} from 'angular2/router';
 
-import {CartoDBService} from '../services/api/cartodb.service';
-import {ApiService} from '../services/api/api.service';
 import {TypeButtonComponent} from '../components/type-button.component';
 import {DateRangeComponent} from '../components/date-range.component';
+import {MapComponent} from '../components/map-component.component';
 
 // Needed to process HTTP requests correctly
 import 'rxjs/Rx';
@@ -13,9 +12,12 @@ import 'rxjs/Rx';
 @Component({
   selector: 'home',
 	templateUrl: 'partials/home.component.html',
-  directives: [ROUTER_DIRECTIVES,
-               TypeButtonComponent,
-               DateRangeComponent]
+  directives: [
+      ROUTER_DIRECTIVES,
+      TypeButtonComponent,
+      DateRangeComponent,
+      MapComponent
+ ]
 })
 export class HomeComponent implements OnInit {
   public routes = Routes;
@@ -24,9 +26,7 @@ export class HomeComponent implements OnInit {
   /** Observable Events */
   // keywordSearchObserver = new EventEmitter();
   constructor(
-      public _cartoDBService: CartoDBService
   ) {
-      _cartoDBService.getData();
   }
 
   ngOnInit() {
--- src/app/services/api/cartodb.service.ts
+++ src/app/services/api/cartodb.service.ts
@@ -1,6 +1,7 @@
 import {Injectable, EventEmitter, Directive, HostListener, OnInit} from 'angular2/core';
 import {Http, Headers} from 'angular2/http';
 import {Router} from 'angular2/router';
+import {DatePipe} from 'angular2/common';
 
 
 // Needed to process HTTP requests correctly
@@ -13,20 +14,14 @@ export class CartoDBService {
 
 	constructor(
         public _router: Router,
-		public _http: Http
+		public _http: Http,
+        public _datePipe: DatePipe
     ) {
 
 	}
 
-  getData() {
-      var sql = new cartodb.SQL({ user: 'cartodb_user' });
-      sql.execute("SELECT * FROM table_name WHERE id > {{id}}", { id: 3 })
-        .done(function(data) {
-          console.log(data.rows);
-        })
-        .error(function(errors) {
-          // errors contains a list of errors
-          console.log("errors:" + errors);
-        })
+  getData(startDate: Date, endDate: Date) {
+      let APIKEY = "59f08e1c8d3bd54c48b73f4d415d2c4b4286e674";
+      return this._http.get(`https://philknight.cartodb.com/api/v2/sql?api_key=${APIKEY}&q=SELECT * FROM banes_environmental_protection_service_requestsv2 WHERE closeddate BETWEEN '2015-05-17T00:46:40' AND '2015-08-26T07:00:00'`);
   }
 }
