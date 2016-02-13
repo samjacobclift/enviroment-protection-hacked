16f219c2eda8baf2dd08ac07cfd9e44adb76e3d0
Sam Clift <sam@cloudfindhq.com>
2016-02-13 11:38:25 +0000

    Basic button dropdown to select types

--- src/app/components/type-button.component.ts
+++ src/app/components/type-button.component.ts
@@ -10,17 +10,41 @@ export class TypeButtonComponent implements OnInit {
   private selectedType: string;
   private showMenu: boolean;
 
+  private reportTypes:Array<string>
+
   constructor() {
   }
 
   ngOnInit() {
     this.selectedType = 'Select One';
     this.showMenu = false;
+    // load the report types
+    this.reportTypes = [
+      'Noise complaint',
+      'Light pollution',
+      'Accumulations (including refuse & litter)',
+      'Nuisance (excluding noise & pollution)',
+      'Drainage Complaint',
+      'Odour/Fumes',
+      'Bonfires',
+      'General (non-noise) complaint',
+      'Rat Complaint'
+    ]
   }
 
   toggleMenu() {
-    console.log('toggling')
     this.showMenu = !this.showMenu;
+  }
+
+  // Set and store the type
+  selectType(type:string) {
+    this.selectedType = type;
+    this.showMenu = false;
+  }
+
+  // reset
+  resetTypes() {
+
   }
 
 }
