bde12d95c157d84ac06817afd2230bf5f44ce267
Sam Clift <sam@cloudfindhq.com>
2016-02-14 10:35:36 +0000

    Style updates to date and type filtering

--- src/app/components/date-range.component.ts
+++ src/app/components/date-range.component.ts
@@ -10,7 +10,9 @@ import {CartoDBService} from "../services/api/cartodb.service";
 export class DateRangeComponent implements OnInit {
 
   private currentViewDate:Date;
-  private currentDateSearchParams:string;
+  private lastMonth:Date;
+  private nextMonth:Date;
+  private filterActive:boolean;
 
 
   constructor(private _cartoDBService: CartoDBService) {
@@ -20,17 +22,44 @@ export class DateRangeComponent implements OnInit {
     // When loading set to current date
     this.currentViewDate = new Date();
     this.setDateSearchString();
+
+    // set to no filter when page loads
+    this.filterActive = false;
+
   }
 
   setDateSearchString() {
-    let month = (this.currentViewDate.getMonth() + 1);
+    let month = this.currentViewDate.getMonth();
     let year = this.currentViewDate.getFullYear()
-    this.currentDateSearchParams =  year + '-' + month
+
+    this.lastMonth = new Date(year, month, 1);
+    this.lastMonth.setMonth(this.lastMonth.getMonth() - 1);
+
+    this.nextMonth = new Date(year, month, 1);
+    this.nextMonth.setMonth(this.lastMonth.getMonth() + 2);
+
+
   }
+
+  /**
+  Reset to show all data
+  */
+  reset() {
+    this.currentViewDate = new Date();
+
+    this.setDateSearchString();
+
+    this._cartoDBService.setMonth.emit({'currentYear': '',
+                                        'currentMonth': ''});
+    // set to no filter when page loads
+    this.filterActive = false;
+
+  }
+
   /**
   Page to the previous month
   */
-  previousMonth() {
+  filterPreviousMonth() {
     // work out the previous month
     this.currentViewDate.setMonth(this.currentViewDate.getMonth() - 1);
 
@@ -42,8 +71,27 @@ export class DateRangeComponent implements OnInit {
     this._cartoDBService.setMonth.emit({'currentYear':currentYear,
                                         'currentMonth': currentMonth});
 
+    this.filterActive = true;
+
   }
 
+  /**
+  Page to the previous month
+  */
+  filterNextMonth() {
+    // work out the previous month
+    this.currentViewDate.setMonth(this.currentViewDate.getMonth() + 1);
+    let currentYear = this.currentViewDate.getFullYear();
+    let currentMonth = this.currentViewDate.getMonth() + 1;
+
+    this.setDateSearchString();
+
+    this._cartoDBService.setMonth.emit({'currentYear':currentYear,
+                                        'currentMonth': currentMonth});
+
+    this.filterActive = true;
+
+  }
 
 
 }
