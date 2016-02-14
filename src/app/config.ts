576c09dc402ea9e0a95fd7c8bf8bcd2fd5c6980d
Byron Williams <byron@swooneditions.com>
2016-02-14 15:09:15 +0000

    Add default route

--- src/app/config.ts
+++ src/app/config.ts
@@ -6,7 +6,7 @@ import {Router} from 'angular2/router';
 export var Routes = {
     slide1: {path: '/intro', as: 'Intro', component: IntroRouteComponent},
     // slide2: {path: '/slide2', as: 'Slide2', component: HomeComponent},
-    dashboard: {path: '/app', as: 'Root', component: HomeComponent},
+    dashboard: {path: '/', as: 'Root', component: HomeComponent},
 };
 
 export const APP_ROUTES = Object.keys(Routes).map(r => Routes[r]);
